452e8fbdb6556476ac99163b8bcbf0caad1a5688
Andrew <phongnguyentuan20@gmail.com>
2021-01-13 22:02:39 -0500

    added terminal linting

--- next.config.js
+++ next.config.js
@@ -3,12 +3,22 @@ const withPlugins = require('next-compose-plugins');
 const bundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const eslintOptions = {
+	extensions: ['js', 'jsx', 'tsx', 'ts'],
+};
 
 const nextConfig = {
-	webpack: (config, { isServer }) => {
+	webpack: (config, { isServer, dev }) => {
 		// Fixes packages that depend on fs/module module
 		if (!isServer) {
 			config.node = { fs: 'empty', module: 'empty' };
+		}
+
+		// For linting in terminal
+		if (dev) {
+			config.plugins.push(new ESLintPlugin(eslintOptions));
 		}
 
 		return config;
