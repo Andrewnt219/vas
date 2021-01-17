ac2be46500a36cd514d5cb4b4ab4c071dd01b195
Andrew <phongnguyentuan20@gmail.com>
2021-01-17 12:28:52 -0500

    wiped project

--- src/components/atoms/SurveySemesterText/SurveySemesterText.tsx
+++
@@ -1,8 +0,0 @@
-import tw, { styled } from 'twin.macro';
-
-type Props = {};
-const SurveySemesterText = styled.span<Props>`
-	${tw`uppercase font-bold text-4xl sm:text-5xl leading-none lg:leading-close tracking-tighter `}
-`;
-
-export default SurveySemesterText;
