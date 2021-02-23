ed9df79e3c991999697807f9ec37013203546c27
Andrew <phongnguyentuan20@gmail.com>
2021-02-22 21:49:19 -0500

    added manifest and favicon

--- frontend/src/components/MainLayout/MainLayout.tsx
+++ frontend/src/components/MainLayout/MainLayout.tsx
@@ -22,29 +22,6 @@ export default function MainLayout({ children, customMeta }: Props) {
 	return (
 		<Container>
 			<Head>
-				<link rel="shortcut icon" href="/favicon.ico" />
-
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d73732" />
-				<meta name="msapplication-TileColor" content="#d73732" />
-				<meta name="theme-color" content="#d73732"></meta>
 				<title>{meta.title}</title>
 				<meta name="robots" content="follow, index" />
 				<meta content={meta.description} name="description" />
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -46,6 +46,29 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel="shortcut icon" href="/favicon.ico" />
+
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d73732" />
+					<meta name="msapplication-TileColor" content="#d73732" />
+					<meta name="theme-color" content="#d73732"></meta>
 					<link
 						rel="preload"
 						href="/fonts/Inter/inter-v3-latin-regular.woff2"
