9333f6f0dab4276f8df965ccec0a72294cfb8d28
Andrew <phongnguyentuan20@gmail.com>
2021-02-27 17:57:56 -0500

    set up form translation

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,6 @@
 import InputGroup, { InputGroupProps } from '@components/InputGroup/InputGroup';
+import type { Translate } from 'next-translate';
+import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 import type { UseFormMethods } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
@@ -9,16 +11,53 @@ type ContactFields = {
 	'last-name': string;
 	phone: string;
 };
+
+// TODO with translation, probably better to move to hooks
+function getTextFields(
+	register: UseFormMethods<ContactFields>['register'],
+	errors: UseFormMethods<ContactFields>['errors'],
+	t: Translate
+): InputGroupProps<ContactFields>['data'][] {
+	return [
+		{
+			labelText: t('contact-us:form.first-name'),
+			name: 'first-name',
+			error: errors['first-name'],
+			register: register({ required: t('contact-us:form.errors.required') }),
+		},
+		{
+			labelText: t('contact-us:form.last-name'),
+			register: register({
+				required: t('contact-us:form.errors.required'),
+			}),
+			name: 'last-name',
+			error: errors['last-name'],
+		},
+		{
+			labelText: t('contact-us:form.phone'),
+			register: register({
+				maxLength: {
+					value: 10,
+					message: t('contact-us:form.errors.maxLength', { length: 10 }),
+				},
+				required: t('contact-us:form.errors.required'),
+			}),
+			name: 'phone',
+			error: errors['phone'],
+		},
+	];
+}
 type Props = { className?: string };
 
 function ContactForm({ className }: Props) {
 	const { register, handleSubmit, errors } = useForm<ContactFields>();
+	const { t } = useTranslation('contact-us');
+
+	const textFields = getTextFields(register, errors, t);
 
 	const onSubmit = handleSubmit((data) => {
 		console.log(data);
 	});
-
-	const textFields = getTextFields(register, errors);
 
 	return (
 		<form onSubmit={onSubmit} noValidate className={className}>
@@ -29,30 +68,6 @@ function ContactForm({ className }: Props) {
 			<button>Submit</button>
 		</form>
 	);
-}
-
-// TODO with translation, probably better to move to hooks
-function getTextFields(
-	register: UseFormMethods<ContactFields>['register'],
-	errors: UseFormMethods<ContactFields>['errors']
-): InputGroupProps<ContactFields>['data'][] {
-	return [
-		{
-			labelText: 'First Name',
-			name: 'first-name',
-			error: errors['first-name'],
-			register: register({ required: 'Required' }),
-		},
-		{
-			labelText: 'Last Name',
-			register: register({
-				maxLength: { value: 4, message: 'Too long' },
-				required: 'Required',
-			}),
-			name: 'last-name',
-			error: errors['last-name'],
-		},
-	];
 }
 
 export default ContactForm;
