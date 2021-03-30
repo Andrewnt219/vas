4c74ac01dc6f7f5a778198cedffbf90f5360e011
Andrew <phongnguyentuan20@gmail.com>
2021-03-29 23:54:55 -0400

    added thank you and eror state for contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -5,19 +5,21 @@ import TextField from '@components/TextField/TextField';
 import axios from 'axios';
 import Trans from 'next-translate/Trans';
 import useTranslation from 'next-translate/useTranslation';
+import { useState } from 'react';
 import { useContactForm } from './hooks/useContactForm';
 
 type FormProps = { className?: string };
 
 function ContactForm({ className }: FormProps) {
 	const [formMethods, textFields] = useContactForm();
+
 	const { t } = useTranslation();
+	const [error, setError] = useState<string>();
 
 	const onSubmit = formMethods.handleSubmit((data) => {
 		axios
 			.post('/api/contact', data)
-			.then((res) => console.log(res))
-			.catch((err) => console.log(err));
+			.catch(() => setError(t`contact-us:form.sent-fail`));
 	});
 
 	return (
@@ -28,74 +30,97 @@ function ContactForm({ className }: FormProps) {
 			className={className}
 			tw="text-body pt-5 pb-10 px-5 rounded-xl bg-white shadow-card space-y-2 md:(text-2xl space-y-4 pt-10 pb-20 px-10) xl:(p-20 rounded-4xl grid grid-cols-2 gap-x-14)"
 		>
-			<Trans
-				i18nKey="contact-us:title"
-				components={[
-					<h2
-						id="contact-form-title"
-						key="h2"
-						tw="text-h3-variants xl:col-span-full"
-					/>,
-					<span key="span" tw="text-primary " />,
-					<br key="span" />,
-				]}
-			/>
+			{formMethods.formState.isSubmitSuccessful ? (
+				<div tw="text-h3-variants col-span-full self-center">
+					<h2>{t`contact-us:submit-response.title`}</h2>
+					<p tw="text-primary">{t`contact-us:submit-response.subtitle`}</p>
+				</div>
+			) : (
+				<>
+					<Trans
+						i18nKey="contact-us:title"
+						components={[
+							<h2
+								id="contact-form-title"
+								key="h2"
+								tw="text-h3-variants xl:col-span-full"
+							/>,
+							<span key="span" tw="text-primary" />,
+							<br key="span" />,
+						]}
+					/>
 
-			<p tw="mb-10! xl:col-span-full">{t`contact-us:subtitle`}</p>
+					<p tw="mb-10! xl:col-span-full">{t`contact-us:subtitle`}</p>
 
-			<InputGroup error={textFields['first-name'].error}>
-				<InputLabel isRequired htmlFor={textFields['first-name'].register.name}>
-					{textFields['first-name'].labelText}
-				</InputLabel>
+					{error && <p tw="text-primary col-span-2">{error}</p>}
 
-				<TextField
-					autoComplete="given-name"
-					{...textFields['first-name'].register}
-				/>
-			</InputGroup>
+					<InputGroup error={textFields['first-name'].error}>
+						<InputLabel
+							isRequired
+							htmlFor={textFields['first-name'].register.name}
+						>
+							{textFields['first-name'].labelText}
+						</InputLabel>
 
-			<InputGroup error={textFields['last-name'].error}>
-				<InputLabel isRequired htmlFor={textFields['last-name'].register.name}>
-					{textFields['last-name'].labelText}
-				</InputLabel>
+						<TextField
+							autoComplete="given-name"
+							{...textFields['first-name'].register}
+						/>
+					</InputGroup>
 
-				<TextField
-					autoComplete="family-name"
-					{...textFields['last-name'].register}
-				/>
-			</InputGroup>
+					<InputGroup error={textFields['last-name'].error}>
+						<InputLabel
+							isRequired
+							htmlFor={textFields['last-name'].register.name}
+						>
+							{textFields['last-name'].labelText}
+						</InputLabel>
 
-			<InputGroup error={textFields['email'].error}>
-				<InputLabel isRequired htmlFor={textFields['email'].register.name}>
-					{textFields['email'].labelText}
-				</InputLabel>
+						<TextField
+							autoComplete="family-name"
+							{...textFields['last-name'].register}
+						/>
+					</InputGroup>
 
-				<TextField autoComplete="email" {...textFields['email'].register} />
-			</InputGroup>
+					<InputGroup error={textFields['email'].error}>
+						<InputLabel isRequired htmlFor={textFields['email'].register.name}>
+							{textFields['email'].labelText}
+						</InputLabel>
 
-			<InputGroup error={textFields['subject'].error}>
-				<InputLabel isRequired htmlFor={textFields['subject'].register.name}>
-					{textFields['subject'].labelText}
-				</InputLabel>
+						<TextField autoComplete="email" {...textFields['email'].register} />
+					</InputGroup>
 
-				<TextField autoComplete="subject" {...textFields['subject'].register} />
-			</InputGroup>
+					<InputGroup error={textFields['subject'].error}>
+						<InputLabel
+							isRequired
+							htmlFor={textFields['subject'].register.name}
+						>
+							{textFields['subject'].labelText}
+						</InputLabel>
 
-			<InputGroup error={textFields['message'].error} tw="col-span-full">
-				<InputLabel htmlFor={textFields['message'].register.name}>
-					{textFields['message'].labelText}
-				</InputLabel>
+						<TextField
+							autoComplete="subject"
+							{...textFields['subject'].register}
+						/>
+					</InputGroup>
 
-				<TextField
-					autoComplete="message"
-					as="textarea"
-					{...textFields['message'].register}
-				/>
-			</InputGroup>
+					<InputGroup error={textFields['message'].error} tw="col-span-full">
+						<InputLabel htmlFor={textFields['message'].register.name}>
+							{textFields['message'].labelText}
+						</InputLabel>
 
-			<Button variant="contain" tw="mt-5!">
-				{t`contact-us:form.submit-button`}
-			</Button>
+						<TextField
+							autoComplete="message"
+							as="textarea"
+							{...textFields['message'].register}
+						/>
+					</InputGroup>
+
+					<Button variant="contain" tw="mt-5!">
+						{t`contact-us:form.submit-button`}
+					</Button>
+				</>
+			)}
 		</form>
 	);
 }
