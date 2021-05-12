e03c24bf3b466dc579e67c7ea80f60e91f2c45a3
Andrew <phongnguyentuan20@gmail.com>
2021-05-11 20:26:06 -0400

    componentized homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,77 +81,99 @@ const Index: VFC<Props> = ({ data, error }) => {
       title={t('home:hero.title')}
       tw="pb-0! mb-12 space-y-12 xl:space-y-32"
     >
-      <section
-        tw="col-span-full grid grid-cols-12 content-start xl:-mt-24"
-        aria-labelledby="hero-title"
-      >
-        <header tw="text-center grid-p-sm  xl:(col-start-2 col-end-6 self-center z-10 width[150%] text-left)">
-          <h1
-            id="hero-title"
-            tw="text-2xl text-primary font-black md:text-4xl lg:text-5xl"
-          >
-            {t('home:hero.title')}
-          </h1>
+      <HeroSection />
 
-          <p tw="mt-md md:text-base xl:(w-2/3)">{t('home:hero.subtitle')}</p>
-
-          {/* TODO translate */}
-          {/* TODO add primary button for read more (scroll down) */}
-          {/* TODO switch find out more to secondary */}
-          <NextLink href="/about-us" passHref>
-            <Button as="a" variant="contain" size="lg" tw="mt-2xl inline-block">
-              Find out more
-            </Button>
-          </NextLink>
-        </header>
-
-        <div tw="col-span-full mt-10 xl:(mt-0 col-start-6 col-end-13 relative top-24)">
-          <EnhancedImage
-            src={require('images/friends-with-books.png')}
-            lqip={require('images/friends-with-books.png?lqip')}
-            alt="A group of friends reading books"
-            width={2995}
-            height={2331}
-            layout="responsive"
-            sizes="50vw"
-          />
-        </div>
-      </section>
-
-      <section
-        css={wrapper}
-        tw=" col-span-full grid grid-cols-1 xl:(grid-cols-3 gap-x-12 items-start)"
-      >
-        <header tw="col-span-full">
-          {/* TODO translation */}
-          <SectionH1>Latest articles</SectionH1>
-        </header>
-
-        <ul
-          aria-label="List of recent articles"
-          tw="col-span-2 space-y-4 md:space-y-7 xl:space-y-12"
-        >
-          <SizesProvider
-            initialContext={getSizes(['90vw', undefined, '1400px'])}
-          >
-            {latestPosts.results.map((post) => (
-              <li key={post.id}>
-                <PostCard.Article post={post} />
-              </li>
-            ))}
-          </SizesProvider>
-        </ul>
-
-        <aside tw="mt-7 col-span-1 grid gap-y-4 md:(gap-7 grid-cols-2) xl:(mt-0 grid-cols-1 gap-12)">
-          <NewsletterBox />
-          <CategoriesBox categories={categoriesWithPosts} />
-          <SocialMediaBox tw="col-span-full" />
-        </aside>
-      </section>
+      <ArticlesSection
+        categoriesWithPosts={categoriesWithPosts}
+        latestPosts={latestPosts}
+      />
 
       {/* TODO load more, swr infinite */}
     </MainLayout>
   );
 };
+
+function HeroSection() {
+  const { t } = useTranslation();
+
+  return (
+    <section
+      tw="col-span-full grid grid-cols-12 content-start xl:-mt-24"
+      aria-labelledby="hero-title"
+    >
+      <header tw="text-center grid-p-sm  xl:(col-start-2 col-end-6 self-center z-10 width[150%] text-left)">
+        <h1
+          id="hero-title"
+          tw="text-2xl text-primary font-black md:text-4xl lg:text-5xl"
+        >
+          {t('home:hero.title')}
+        </h1>
+
+        <p tw="mt-md md:text-base xl:(w-2/3)">{t('home:hero.subtitle')}</p>
+
+        {/* TODO translate */}
+        {/* TODO add primary button for read more (scroll down) */}
+        {/* TODO switch find out more to secondary */}
+        <NextLink href="/about-us" passHref>
+          <Button as="a" variant="contain" size="lg" tw="mt-2xl inline-block">
+            Find out more
+          </Button>
+        </NextLink>
+      </header>
+
+      <div tw="col-span-full mt-10 xl:(mt-0 col-start-6 col-end-13 relative top-24)">
+        <EnhancedImage
+          src={require('images/friends-with-books.png')}
+          lqip={require('images/friends-with-books.png?lqip')}
+          alt="A group of friends reading books"
+          width={2995}
+          height={2331}
+          layout="responsive"
+          sizes="50vw"
+        />
+      </div>
+    </section>
+  );
+}
+
+type ArticlesSectionProps = {
+  latestPosts: PrismicResult<Post>;
+  categoriesWithPosts: CategoryWithPosts[];
+};
+function ArticlesSection({
+  latestPosts,
+  categoriesWithPosts,
+}: ArticlesSectionProps) {
+  return (
+    <section
+      css={wrapper}
+      tw=" col-span-full grid grid-cols-1 xl:(grid-cols-3 gap-x-12 items-start)"
+    >
+      <header tw="col-span-full">
+        {/* TODO translation */}
+        <SectionH1>Latest articles</SectionH1>
+      </header>
+
+      <ul
+        aria-label="List of recent articles"
+        tw="col-span-2 space-y-4 md:space-y-7 xl:space-y-12"
+      >
+        <SizesProvider initialContext={getSizes(['90vw', undefined, '1400px'])}>
+          {latestPosts.results.map((post) => (
+            <li key={post.id}>
+              <PostCard.Article post={post} />
+            </li>
+          ))}
+        </SizesProvider>
+      </ul>
+
+      <aside tw="mt-7 col-span-1 grid gap-y-4 md:(gap-7 grid-cols-2) xl:(mt-0 grid-cols-1 gap-12)">
+        <NewsletterBox />
+        <CategoriesBox categories={categoriesWithPosts} />
+        <SocialMediaBox tw="col-span-full" />
+      </aside>
+    </section>
+  );
+}
 
 export default Index;
