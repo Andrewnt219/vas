fc953a289575bbcf4e7b3622511067355dd4c228
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2021-05-17 03:25:17 -0400

    Styled /summer-orientation-2021 (#7)
    styled mobile
    
    styled tablet
    
    styled desktop

---
+++ src/utils/date-utils.ts
@@ -0,0 +1,11 @@
+export function getCountdown(distance: number) {
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+}
