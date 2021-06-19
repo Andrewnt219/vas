2e85795707f177468e6cd93a67d9880e51309ae9
Andrew <phongnguyentuan20@gmail.com>
2021-06-19 16:44:55 -0400

    fixed links cause overflow in posts

--- src/components/common/StyledLink/StyledLink.tsx
+++ src/components/common/StyledLink/StyledLink.tsx
@@ -1,31 +1,31 @@
 import tw, { css, styled, theme } from 'twin.macro';
 
 const visitedCss = css`
-	svg {
-		color: ${theme`textColor.skin.muted`};
-	}
+  svg {
+    color: ${theme`textColor.skin.muted`};
+  }
 
-	//NOTE Must be placed at the bottom because weird TS bug
-	&:visited {
-		// NOTE somehow text-skin-muted isn't working
-		color: ${theme`textColor.skin.muted`};
-		${tw`decorator-current`};
-	}
+  //NOTE Must be placed at the bottom because weird TS bug
+  &:visited {
+    // NOTE somehow text-skin-muted isn't working
+    color: ${theme`textColor.skin.muted`};
+    ${tw`decorator-current`};
+  }
 `;
 
 const hocusCss = css`
-	&:hover,
-	&:focus,
-	&:visited:hover,
-	&:visited:focus {
-		${tw`text-primary`}
-		${tw`svg:(transform text-primary scale-125)`}
-	}
+  &:hover,
+  &:focus,
+  &:visited:hover,
+  &:visited:focus {
+    ${tw`text-primary`}
+    ${tw`svg:(transform text-primary scale-125)`}
+  }
 `;
 
 const StyledLink = styled.a`
-	${tw`underline transition-colors decorator-primary`}
-	${tw`svg:(transition-all ease-linear text-primary ml-2 text-smaller)`}
+  ${tw`underline transition-colors decorator-primary break-all`}
+  ${tw`svg:(transition-all ease-linear text-primary ml-2 text-smaller)`}
 	${tw`inline-flex items-center cursor-pointer`}
  
 	${visitedCss}
